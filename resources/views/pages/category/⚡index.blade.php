faba317c7092e2c97bb03fcd5931bcb518aea8e4
haji123456789131 <parisjibron.240604075@student.hamzanwadi.ac.id>
2026-06-20 15:25:24 +0800

    update category paris

---
+++ resources/views/pages/category/⚡index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    <h1>Halo bosque</h1>
+    {{-- Very little is needed to make a happy life. - Marcus Aurelius --}}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::livewire('categories', 'pages::category.index')->name('categories.index');
 });
 
 require __DIR__.'/settings.php';
